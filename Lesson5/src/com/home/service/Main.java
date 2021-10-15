5e1cd1e09a7a40ea81058e98523b38e7932c29dd
MyName <kolosovichulia@mail.ru>
2021-10-15 22:51:30 +0300

    Урок 5 Исправила

--- Lesson5/src/com/home/service/Main.java
+++ Lesson5/src/com/home/service/Main.java
@@ -10,25 +10,13 @@ public class Main {
         int ram = 8;
         String hardDrive = "SSD";
         int workCycle = 6;
-        int number = 1;
-        Scanner scanner = new Scanner(System.in);
 
         Computer comp = new Computer(interpreter, ram, hardDrive, workCycle);
 
         //System.out.println(comp.toString());
 
-while(number == 1) {
-    System.out.println("Вы хотите включить компьютер снова? Введите 1, если  да: ");
-
-    while (!scanner.hasNextInt()) {
-        System.out.println("Вы ввели не число!");
-        scanner.next();
-    }
-    number = scanner.nextInt();
-
-    if (number == 1) {
-        comp.on();
-    } else {number = 0;}
-}
+        for (int i = 0; i < workCycle; i++) {
+            comp.on();
+        }
     }
 }
