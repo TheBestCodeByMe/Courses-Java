8642e295968fce2645d35c992bcf0bbf855b161c
MyName <kolosovichulia@mail.ru>
2021-10-24 21:17:29 +0300

    Урок 6 Исправила

--- Lesson6/src/com/home/utils/Constants.java
+++ Lesson6/src/com/home/utils/Constants.java
@@ -5,7 +5,7 @@ import com.home.model.Person;
 import java.util.List;
 
 public class Constants {
-    public static final int minAge = 18;
-    public static final int maxAge = 27;
-    public static final String validSex = "Мужчина";
+    public static final int MIN_AGE = 18;
+    public static final int MAX_AGE = 27;
+    public static final String VALID_SEX = "Мужчина";
 }
