a2d0bf430d7cc1206a53caed8d4df7a69ba9bd01
MyName <kolosovichulia@mail.ru>
2021-11-02 22:33:41 +0300

    Урок 7 Аа, я уже не могу видеть эту машину, аххах, я переделала заново, убрала весь статик, но теперь новая проблема, как его создать в мейне, если конструктор требует инициализации этого поля файнал. как это можно обойти? Или убрать из другого конструктора машины двигатель. И создать отдельно этот двигатель. Ох уж эти внутренние классы..

--- Lesson7/src/main/java/Main.java
+++ Lesson7/src/main/java/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        Car car1 = new Car(car.new Engine("engine", "type"), car.new GasTank(60, 100));
-        Car.GasTank gas = car.new GasTank();
+        Car1 car;// = new Car1();
+        Car1.GasTank gasTank = car.new GasTank(60, 100);
+        Car1 car1 = new Car1(car.new Engine("engine", "type"), gasTank);
+        Car1.GasTank gas = car.new GasTank();
         car.startCar();
         car.turnOffCar();
         car.viewDistance();
@@ -12,12 +13,13 @@ public class Main {
         //engine.setType("type1");
         //engine.viewType();
         //gas.viewFuel();
-        Car.GasTank.setFuel(90);
+        gasTank.setFuel(90);
         car.viewFuel();
         //Car.Engine.viewType();
         //Car.GasTank.viewFuel();
         //Car.Engine.setType("type1");
         //Car.Engine.viewType();
-        Car.GasTank.setFuel(90);
+        gasTank.setFuel(90);
+
     }
 }
